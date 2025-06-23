0e8d0611216f932e6f3ca264abd870829a8dd846
Ayushi-shukla90 <ayushishukla66237@gmail.com>
2025-06-23 16:31:37 +0530

    code

--- src/main.tsx
+++ src/main.tsx
@@ -6,3 +6,26 @@ const rootElement = document.getElementById("root");
 if (rootElement) {
   createRoot(rootElement).render(<App />);
 }
+
+import * as faceapi from 'face-api.js';
+
+async function loadModels() {
+  const MODEL_URL = '/models';
+  await faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL);
+  await faceapi.nets.faceLandmark68Net.loadFromUri(MODEL_URL);
+  await faceapi.nets.faceRecognitionNet.loadFromUri(MODEL_URL);
+  await faceapi.nets.ageGenderNet.loadFromUri(MODEL_URL); // Needed for gender detection
+}
+
+async function detectGender(input: HTMLImageElement | HTMLVideoElement) {
+  const detections = await faceapi
+    .detectAllFaces(input, new faceapi.TinyFaceDetectorOptions())
+    .withFaceLandmarks()
+    .withAgeAndGender();
+
+  detections.forEach(result => {
+    const { gender, genderProbability } = result;
+    console.log(`Gender: ${gender}, Probability: ${genderProbability}`);
+    // You can display this info on your UI as needed
+  });
+}
